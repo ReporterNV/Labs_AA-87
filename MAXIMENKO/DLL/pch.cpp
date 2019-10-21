79e7681957cee57816ac5cfe0c33d523ead7d320
ReporterNV <30627298+ReporterNV@users.noreply.github.com>
2019-10-21 22:22:36 +0700

    Add files via upload

--- MAXIMENKO/DLL/pch.cpp
+++ MAXIMENKO/DLL/pch.cpp
@@ -4,12 +4,14 @@
 #define BUF_SIZE 256
 
 void MAXIMENKO(char* a, int* b, int c) {
-	for (DWORD i = 0; i < c; i++) { //обработка строки
-		if (int(a[i]) < 48) {
-
+	*b = 0;
+	for (int i = 0; i < c; i++) { //обработка строки
+		if (a[i] < 48) {
+			
 			a[i] = ' ';
+			
+			*b = *b + 1;
 
-			*b++;
 
 		}
 
